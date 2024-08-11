aa35bebf896c3ce3a89b5a7681390faeeef2eb9d
João Victor Souza <jvsouzasantos@id.uff.br>
2024-08-10 22:51:11 -0300

    refactor: methods names and images

--- main.py
+++ main.py
@@ -4,54 +4,53 @@ import pyautogui
 import mss
 from time import sleep 
 
-def capturar_tela():
+def capture_screen():
     with mss.mss() as sct:
         screenshot = sct.grab(sct.monitors[0])
         screenshot_np = np.array(screenshot)
         gray_screenshot = cv2.cvtColor(screenshot_np, cv2.COLOR_BGRA2GRAY)
     return gray_screenshot
 
-def carregar_template(template_path):
+def load_template(template_path):
     template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
-    largura, altura = template.shape[::-1]
-    return template, largura, altura
+    width, height = template.shape[::-1]
+    return template, width, height
 
-def encontrar_template(gray_screenshot, template):
+def find_template(gray_screenshot, template):
     res = cv2.matchTemplate(gray_screenshot, template, cv2.TM_CCOEFF_NORMED)
     _, _, _, max_loc = cv2.minMaxLoc(res)
     return max_loc
 
-def clicar(top_left, largura, altura):
-    center_x = top_left[0] + largura // 2
-    center_y = top_left[1] + altura // 2
+def click(top_left, width, height):
+    center_x = top_left[0] + width // 2
+    center_y = top_left[1] + height // 2
     pyautogui.click(center_x, center_y)
 
-#Abri o navegador
-template, largura, altura = carregar_template('templates/chrome.png')
-gray_screenshot = capturar_tela()
-top_left = encontrar_template(gray_screenshot, template)
-clicar(top_left, largura, altura)
+# Open the browser
+template, width, height = load_template('templates/chrome.png')
+gray_screenshot = capture_screen()
+top_left = find_template(gray_screenshot, template)
+click(top_left, width, height)
 
-# Abrir nova aba
+# Open a new tab
 sleep(1)
 pyautogui.hotkey('ctrl', 't')
-# Ir para aba do youtube music
+# Go to the YouTube Music tab
 pyautogui.write('https://music.youtube.com/')
 pyautogui.press('enter')
 
-
 sleep(5)
-# Pesquisar música
-template, largura, altura = carregar_template('templates/pesquisa.png')
-gray_screenshot = capturar_tela()
-top_left = encontrar_template(gray_screenshot, template)
-clicar(top_left, largura, altura)
+# Search for music
+template, width, height = load_template('templates/search_bar.png')
+gray_screenshot = capture_screen()
+top_left = find_template(gray_screenshot, template)
+click(top_left, width, height)
 pyautogui.write('Shape of You')
 pyautogui.press('enter')
 
-# Clicar na música
+# Click on the music
 sleep(5)
-template, largura, altura = carregar_template('templates/musica.png')
-gray_screenshot = capturar_tela()
-top_left = encontrar_template(gray_screenshot, template)
-clicar(top_left, largura, altura)
+template, width, height = load_template('templates/music.png')
+gray_screenshot = capture_screen()
+top_left = find_template(gray_screenshot, template)
+click(top_left, width, height)
